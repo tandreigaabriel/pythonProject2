0a0202fc14315fbafa649b56c7561fd63115334d
Toma Andrei G <56647464+tandreigaabriel@users.noreply.github.com>
2022-09-14 05:27:16 +0100

    Automation with Python

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def process_workbook(filename):
 
     for row in range(2, sheet.max_row + 1):
         cell = sheet.cell(row,3)
-        corrected_price = cell.value * 0.9
+        corrected_price = cell.value * 0.99
         corrected_price_cell = sheet.cell(row,4)
         corrected_price_cell.value = corrected_price
     values = Reference(sheet,
